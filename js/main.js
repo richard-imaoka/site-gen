d7e193167d0ea876638fed10e1cc1fd9fa89c596
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-01-22 00:31:02 +0900

    Renewal!

--- js/main.js
+++ js/main.js
@@ -16,6 +16,14 @@ jQuery(function($) {'use strict',
 
 	 	$(this).closest('.panel-heading').toggleClass('active');
 	});
+	// accordian
+	$('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
 
 	//Initiat WOW JS
 	new WOW().init();
